ebe53162150fa671f559d3daf09749fe4ece8fbe
ztarikul <ztarikulislam.com>
2022-03-07 12:14:00 +0600

    component tree build

---
+++ src/components/MainComponent.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Body from "./body/Body";
+
+
+const MainComponent = () => {
+    return (
+        <div>
+            <Header />
+            <Footer />
+            <Body />
+
+        </div>
+    );
+}
+
+export default MainComponent;
